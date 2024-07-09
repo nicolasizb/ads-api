d97031bb3484c2e64146c4bca91eb835d12fd27c
nicolasizb <nicolasizquierdocontacto@gmail.com>
2024-07-09 16:38:57 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MainComponent } from './components/main/main.component';
+import { CampaignCardComponent } from './components/campaign-card/campaign-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CampaignCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/campaign-card/campaign-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-card',
+  templateUrl: './campaign-card.component.html',
+  styleUrls: ['./campaign-card.component.scss']
+})
+export class CampaignCardComponent {
+
+}
